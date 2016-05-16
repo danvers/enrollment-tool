26ddb08c5f7cb62ccb8a3461c7a1d3604f9b5008
danvers <dan@pixelspace.org>
2016-05-16 18:08:23 +0200

    updated welcome message

--- app/language/de/Home.php
+++ app/language/de/Home.php
@@ -4,8 +4,8 @@ return array(
 
     //index
     'home_text' => 'Willkommen',
-    'home_message' => 'Hallo und Willkommen! Hier können Sie sich für Veranstaltungen des <a href="http://www.meb.ovgu.de/">Lehrstuhls für Medien- und Erwachsenenbildung</a> sowie des <a href="http://www.ovgu.de/iniew/">Lehrstuhls für Allgemeine Pädagogik</a> anmelden. Weitere Informationen zu anderen Lehrangeboten finden Sie im <a href="https://lsf.ovgu.de">Portal für Lehre Studium und Forschung (LSF)</a>. <br /><br />Für Fragen und Probleme bei der Anmeldung können Sie sich an <a href="http://www.ovgu.de/iniew/team/verstaendig">Dan Verständig</a> oder <a href="https://www.meb.ovgu.de/mitarbeiter/dr-ralf-biermann/">Dr. Ralf Biermann</a> wenden.',
-    'no_entries' => 'Derzeit stehen keine Kurse für eine Einschreibung zur Verfügung. Die Anmeldungen für das Sommersemester 2016 sind vom 14.-27. März 2016 geöffnet.',
+    'home_message' => 'Hallo und Willkommen!',
+    'no_entries' => 'Derzeit stehen keine Kurse für eine Einschreibung zur Verfügung.',
     'th_title' => 'Titel',
     'th_lk' => 'Lehrkraft',
     'th_entries' => 'Anmeldungen',
